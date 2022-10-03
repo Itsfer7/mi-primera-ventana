c1eeaba4e97dccdf2fb9554c5c3eea0c83aee4fa
Ivan <ivanfergarcia@gmail.com>
2022-10-03 17:13:16 +0200

    Actualización 03/10/22

--- src/org/iesfm/ventana/EjemploGridBagLayout4.java
+++ src/org/iesfm/ventana/EjemploGridBagLayout4.java
@@ -6,38 +6,38 @@ import java.awt.*;
 public class EjemploGridBagLayout4 {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setBounds(10,10,400,400);
+        frame.setBounds(10,10,500,400);
         frame.setVisible(true);
         frame.setTitle("Ejemplo 2 GridBagLayout");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel mainPanel = new JPanel();
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10,30,10,30));
         GridBagLayout gridBagLayout = new GridBagLayout();
         mainPanel.setLayout(gridBagLayout);
 
         JTextField fullNameTextField = new JTextField();
         GridBagConstraints constraintsFullNameTextField = new GridBagConstraints(
-                1,0,3,1,1,1,
-                GridBagConstraints.CENTER,
+                1,0,5,1,1,0,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.HORIZONTAL,
-                new Insets(0,0,0,0),
+                new Insets(0,0,5,0),
                 0,0
         );
 
         JTextField addressTextField = new JTextField();
         GridBagConstraints constraintsAddressTextField = new GridBagConstraints(
-                1,2,1,1,1,1,
-                GridBagConstraints.CENTER,
+                1,2,3,1,3,0,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.HORIZONTAL,
-                new Insets(0,0,0,0),
+                new Insets(0,0,0,5),
                 0,0
         );
 
         JTextField cityTextField = new JTextField();
         GridBagConstraints constraintsCityTextField = new GridBagConstraints(
-                2,2,1,1,0,1,
-                GridBagConstraints.CENTER,
+                4,2,1,1,1,0,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.HORIZONTAL,
                 new Insets(0,0,0,0),
                 0,0
@@ -46,122 +46,122 @@ public class EjemploGridBagLayout4 {
 
         JTextField zipCodeTextField = new JTextField();
         GridBagConstraints constraintsZipCodeTextField = new GridBagConstraints(
-                3,2,1,1,0,1,
-                GridBagConstraints.CENTER,
+                5,2,1,1,1,0,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.HORIZONTAL,
-                new Insets(0,0,0,0),
+                new Insets(0,5,0,0),
                 0,0
         );
 
 
         JTextField phoneTextField = new JTextField();
         GridBagConstraints constraintsPhoneTextField = new GridBagConstraints(
-                1,3,1,1,1,1,
-                GridBagConstraints.CENTER,
+                1,3,3,1,3,0,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.HORIZONTAL,
-                new Insets(0,0,0,0),
+                new Insets(0,0,8,5),
                 0,0
         );
 
 
         JTextField ageTextField = new JTextField();
         GridBagConstraints constraintsAgeTextField = new GridBagConstraints(
-                3,3,1,1,0,1,
-                GridBagConstraints.CENTER,
+                5,3,1,1,1,0,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.HORIZONTAL,
-                new Insets(0,0,0,0),
+                new Insets(0,5,0,0),
                 0,0
         );
 
 
         JTextArea descriptionTextArea = new JTextArea();
         GridBagConstraints constraintsDescriptionTextArea = new GridBagConstraints(
-                1,4,2,2,1,2,
-                GridBagConstraints.CENTER,
+                1,4,5,1,1,25,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.BOTH,
                 new Insets(0,0,0,0),
                 0,0
         );
 
 
-        JLabel fullNameLabel = new JLabel("Full Name");
+        JLabel fullNameLabel = new JLabel("Full Name", SwingConstants.RIGHT);
         GridBagConstraints constraintsFullNameLabel = new GridBagConstraints(
-                0,0,1,1,1,1,
-                GridBagConstraints.CENTER,
+                0,0,1,1,1,0,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.HORIZONTAL,
-                new Insets(0,0,0,0),
+                new Insets(0,0,0,5),
                 0,0
         );
 
-        JLabel addressLabel = new JLabel("Address");
+        JLabel addressLabel = new JLabel("Address", SwingConstants.RIGHT);
         GridBagConstraints constraintsAddressLabel = new GridBagConstraints(
                 0,2,1,1,1,1,
-                GridBagConstraints.CENTER,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.HORIZONTAL,
-                new Insets(0,0,0,0),
+                new Insets(0,0,0,5),
                 0,0
         );
 
-        JLabel streetLabel = new JLabel("Street");
+        JLabel streetLabel = new JLabel("Street", SwingConstants.CENTER);
         GridBagConstraints constraintsStreetLabel = new GridBagConstraints(
-                1,1,1,1,1,1,
-                GridBagConstraints.CENTER,
+                1,1,3,1,1,0,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.HORIZONTAL,
                 new Insets(0,0,0,0),
                 0,0
         );
 
-        JLabel cityLabel = new JLabel("City");
+        JLabel cityLabel = new JLabel("City", SwingConstants.CENTER);
         GridBagConstraints constraintsCityLabel = new GridBagConstraints(
-                2,1,1,1,1,1,
-                GridBagConstraints.CENTER,
+                4,1,1,1,1,0,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.HORIZONTAL,
                 new Insets(0,0,0,0),
                 0,0
         );
 
-        JLabel zipCodeLabel = new JLabel("Zip Code");
+        JLabel zipCodeLabel = new JLabel("Zip Code", SwingConstants.CENTER);
         GridBagConstraints constraintsZipCodeLabel = new GridBagConstraints(
-                3,1,1,1,1,1,
-                GridBagConstraints.CENTER,
+                5,1,1,1,1,0,
+                GridBagConstraints.NORTH,
+                GridBagConstraints.HORIZONTAL,
+                new Insets(0,5,0,0),
+                0,0
+        );
+
+        JLabel phoneLabel = new JLabel("Phone", SwingConstants.RIGHT);
+        GridBagConstraints constraintsPhoneLabel = new GridBagConstraints(
+                0,3,1,1,1,0,
+                GridBagConstraints.NORTH,
+                GridBagConstraints.HORIZONTAL,
+                new Insets(0,0,0,5),
+                0,0
+        );
+
+        JLabel ageLabel = new JLabel("Age", SwingConstants.RIGHT);
+        GridBagConstraints constraintsAgeLabel = new GridBagConstraints(
+                4,3,1,1,1,0,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.HORIZONTAL,
                 new Insets(0,0,0,0),
                 0,0
         );
 
-        JLabel phoneLabel = new JLabel("Phone");
-        GridBagConstraints constraintsPhoneLabel = new GridBagConstraints(
-                0,3,1,1,1,1,
-                GridBagConstraints.CENTER,
-                GridBagConstraints.BOTH,
-                new Insets(0,0,0,0),
-                0,0
-        );
-
-        JLabel ageLabel = new JLabel("Age");
-        GridBagConstraints constraintsAgeLabel = new GridBagConstraints(
-                2,3,1,1,1,1,
-                GridBagConstraints.CENTER,
-                GridBagConstraints.BOTH,
-                new Insets(0,0,0,0),
-                0,0
-        );
-
-        JLabel descriptionLabel = new JLabel("Description");
+        JLabel descriptionLabel = new JLabel("Description", SwingConstants.RIGHT);
         GridBagConstraints constraintsDescriptionLabel = new GridBagConstraints(
-                0,4,1,1,1,1,
-                GridBagConstraints.CENTER,
-                GridBagConstraints.BOTH,
-                new Insets(0,0,0,0),
+                0,4,1,1,1,0,
+                GridBagConstraints.NORTH,
+                GridBagConstraints.HORIZONTAL,
+                new Insets(0,0,0,5),
                 0,0
         );
 
         JButton submitButton = new JButton("Submit");
         GridBagConstraints constraintsSubmitButton = new GridBagConstraints(
-                2,5,2,1,1,1,
-                GridBagConstraints.CENTER,
+                4,5,2,1,1,0,
+                GridBagConstraints.NORTH,
                 GridBagConstraints.HORIZONTAL,
-                new Insets(0,0,0,0),
+                new Insets(5,0,0,0),
                 0,0
         );
 
